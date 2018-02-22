c1a108abe49d1d164ddbf434a8c8298fe2e081f6
Krista Moroder <kmoroder@gmail.com>
2018-02-22 18:50:50 +0900

    add conditionals

---
+++ weekOne/thursday/introToConditionals/script.js
@@ -0,0 +1,74 @@
+/*/////////////////////////////////////////
+TASK ONE
+
+Add a conditional to function below so that it
+prints which number is bigger to the console.
+
+console.log('the first argument is bigger');
+console.log('the second argument is bigger');
+///////////////////////////////////////*/
+
+function biggerThan(numOne, numTwo) {
+
+}
+
+biggerThan(4, 3);
+
+/*/////////////////////////////////////////
+TASK TWO
+
+Create a function that prints the data type
+that is passed in as an argument. You will use
+a new JavaScript operator for this: 'typeof'.
+
+You can learn more about this operator here:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+The first has been done for you below.
+
+function whatDataType(data) {
+  if (typeof data === 'string') {
+    console.log('This is a string');
+  }
+}
+
+whatDataType('Hello!'); // should print 'This is a string'
+whatDataType(true); // should print 'This is a boolean'
+whatDataType(42); // should print 'This is a number'
+whatDataType(undefined) // should print 'This is not a string, boolean, or number'
+///////////////////////////////////////*/
+
+
+
+/*/////////////////////////////////////////
+TASK THREE
+
+Create a function called 'greeting' that takes
+in two parameters: a name and a language. It should
+greet the person in the language they have specified.
+
+Here are some examples:
+greeting('Harry Potter', 'Japanese'); // should print 'Konnichiwa, Harry Potter!'
+greeting('Harry Potter', 'English'); // should print 'Hello Harry Potter!'
+greeting('Harry Potter', 'German'); // should print 'Gutentag, Harry Potter!'
+greeting('Harry Potter', 'Spanish'); // should print 'Hola, Harry Potter!'
+///////////////////////////////////////*/
+
+
+
+/*/////////////////////////////////////////
+TASK FOUR
+
+Create a function that prints
+whether a number is even or odd.
+
+Hint: for this, you will want to use the
+modulo operator (%), which returns the remainder
+of dividing one number by another number.
+
+Bonus: add a conditional before everything else
+that prints 'this is not a number' if the argument
+passed in is not a number.
+///////////////////////////////////////*/
+
+
